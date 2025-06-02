d9888eee3cea8feac13678655f0121807d651538
Adil omer <36453919+adelazhary@users.noreply.github.com>
2025-06-02 11:24:55 +0800

    fix: update profile controller and other files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,29 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {createApp, h} from 'vue';
+import {ZiggyVue} from 'ziggy-js';
+import MainLayout from './Layouts/MainLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: (name) => {
+        const page = resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+            import.meta.glob('./Pages/**/*.vue')
+        );
+
+        return page.then((module) => {
+            module.default.layout = module.default.layout || MainLayout;
+            return module;
+        });
+    },
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
@@ -24,4 +31,7 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+}).then(r => {
+    r.app.component('MainLayout', MainLayout);
 });
+
