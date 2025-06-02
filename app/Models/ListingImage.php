1333a7255912e50626eff4e25a188afc1e4b7a7b
Adil omer <36453919+adelazhary@users.noreply.github.com>
2025-06-02 11:24:05 +0800

    feat(models): implement listing, offer, and image models with migrations

---
+++ app/Models/ListingImage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ListingImageFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ListingImage extends Model
+{
+    /** @use HasFactory<ListingImageFactory> */
+    use HasFactory;
+
+    protected $fillable = ['filename'];
+    protected $appends = ['src'];
+
+    /**
+     * Get the listing that owns the ListingImage
+    */
+    public function listing(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class);
+    }
+
+    // getRealSrcAttribute -> real_src
+    public function getSrcAttribute(): string
+    {
+        return asset("storage/$this->filename");
+    }
+}
