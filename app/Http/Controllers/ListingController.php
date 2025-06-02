68b716e7fac84d741b7bd4cba0d28770a5e3abce
Adil omer <36453919+adelazhary@users.noreply.github.com>
2025-06-02 11:48:27 +0800

    Update files

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use App\Http\Requests\StoreListingRequest;
+use App\Http\Requests\UpdateListingRequest;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Response;
+
+
+class ListingController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): Response
+    {
+        Gate::authorize(
+            'viewAny',
+            Listing::class
+        );
+        $filters = $request->only([
+            'priceFrom',
+            'priceTo',
+            'beds',
+            'baths',
+            'areaFrom',
+            'areaTo'
+        ]);
+
+        return Inertia::render(
+            'Listing/Index',
+            [
+                'filters' => $filters,
+                'listings' => Listing::mostRecent()
+                    ->filter($filters)
+                    ->withoutSold()
+                    ->paginate(10)
+                    ->withQueryString()
+            ]
+        );
+    }
+    /**
+     * Display the specified resource.
+     */
+
+    public function show(Listing $listing): Response
+    {
+        Gate::authorize(
+            'view',
+            $listing
+        );
+        $listing->load(['images']);
+        $offer = !Auth::user() ?
+            null : $listing->offers()->byMe()->first();
+
+        return Inertia::render(
+            'Listing/Show',
+            [
+                'listing' => $listing,
+                'offerMade' => $offer
+            ]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreListingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Listing $listing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateListingRequest $request, Listing $listing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Listing $listing)
+    {
+        //
+    }
+}
