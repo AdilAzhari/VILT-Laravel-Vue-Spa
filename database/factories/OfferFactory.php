ac76854540b8e89da32ac1039f30b645624381b8
Adil omer <36453919+adelazhary@users.noreply.github.com>
2025-06-08 17:03:36 +0800

    Enhance listing factories with relationships and states

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Listing;
+use App\Models\Offer;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Offer>
+ * @extends Factory<Offer>
  */
 class OfferFactory extends Factory
 {
@@ -17,7 +20,22 @@ class OfferFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'amount' => $this->faker->numberBetween(100000, 500000),
+            'listing_id' => Listing::factory(),
+            'bidder_id' => User::factory(),
         ];
+    }
+
+    /**
+     * @return OfferFactory|Factory
+     */
+
+    public function lowerOffer(): OfferFactory|Factory
+    {
+        return $this->state(function () {
+            return [
+                'amount' => $this->faker->numberBetween(50000, 99999),
+            ];
+        });
     }
 }
